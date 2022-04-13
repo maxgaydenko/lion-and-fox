958641d8403556d1d6ede0226602acd12654989f
Max Gaydenko <max@gaydenko.ru>
2022-04-14 00:18:15 +0300

    Add conic/menu rotation

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -10,8 +10,8 @@ export const Sidebar: React.FC<IProps> = (props: IProps) => {
    <div className="logo">logo</div>
    <div className="icons">
     <ul>
-     <li className="menu-item"><a className="icon icon-menu" href="#" onClick={props.onMenuClick}></a></li>
-     <li className="other-item"><a className="icon icon-instagram" href="#instagram"></a></li>
+     <li className="menu-item"><button className="icon icon-menu" onClick={props.onMenuClick} /></li>
+     <li className="other-item"><a className="icon icon-instagram" href="https://instagram.com" target="_blank"></a></li>
      <li className="other-item"><a className="icon icon-mail" href="mailto:mail@gaydenko.ru"></a></li>
     </ul>
    </div>
