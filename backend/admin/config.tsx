9fe61a67f49bfa272f0c1e622d2334941f7af62b
Max Gaydenko <max@gaydenko.ru>
2022-04-19 07:29:10 +0300

    custom logo

--- backend/admin/config.tsx
+++ backend/admin/config.tsx
@@ -1,5 +1,11 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@keystone-ui/core';
+
+const p = require('../package.json');
+
 function CustomLogo () {
- return <h3>Lion and fox</h3>
+    return <h3>Lion and fox <div css={{fontSize: '10px'}}>{p.version}</div></h3>
 }
 
 export const components = {
