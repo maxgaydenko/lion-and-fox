f95bd87a517a1ffff429b1dc7882959a9a94914d
Max Gaydenko <max@gaydenko.ru>
2022-04-21 06:09:39 +0300

    Add package.json npm run migrate command

--- backend/README.md
+++ backend/README.md
@@ -1,2 +1,10 @@
 # Lionandfox keystone backend
 
+## env
+
+```
+PORT=3030
+SESSION_SECRET=SomeSecrectStringForSessionMin32Chars
+FILES_STORAGE_PATH=/path/to/files/storage/dir
+FILES_BASE_URL=/files/url/prefix
+```
--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,37 +1,58 @@
-import { config } from '@keystone-6/core';
-import { lists } from './schema';
-import { withAuth, session } from './auth';
-import express from 'express';
+import { config } from "@keystone-6/core";
+import { lists } from "./schema";
+import { withAuth, session } from "./auth";
+import express from "express";
+
+const getProductionEnv = (envVariable: string, defaultValue: string): string => {
+ if (process.env.NODE_ENV === "production") {
+  const variable = process.env[envVariable];
+  if (!variable) throw new Error(`The ${envVariable} environment variable must be set in production`);
+  return variable;
+ }
+ return defaultValue;
+};
+
+// let dbFile = process.env.DB_FILE;
+// if (!dbFile) {
+//  if (process.env.NODE_ENV === "production") {
+//   throw new Error("The DB_FILE environment variable must be set in production");
+//  } else {
+//   dbFile = "./keystone.db";
+//  }
+// }
+
+const filesStoragePath = getProductionEnv("FILES_STORAGE_PATH", "../storage_files/");
+const filesBaseUrl = getProductionEnv("FILES_BASE_URL", "/static_files");
 
 export default withAuth(
-  config({
-    server: {
-      cors: true,
-      extendExpressApp: (app, createContext) => {
-        app.use('/upload_files', express.static('./upload_files'));
-        app.get('/api/users', async (req, res) => {
-          const context = await createContext(req, res);
-          const users = await context.query.User.findMany();
-          res.json(users);
-        });
-      },
-    },
-    files: {
-      upload: 'local',
-      local: {
-        storagePath: './upload_files/',
-        baseUrl: '/upload_files',
-      },
-    },
-    db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
-      useMigrations: true,
-    },
-    ui: {
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
-    lists,
-    session,
-  })
+ config({
+  server: {
+   cors: true,
+   extendExpressApp: (app, createContext) => {
+    app.use(filesBaseUrl, express.static(filesStoragePath));
+    // app.get("/api/users", async (req, res) => {
+    //  const context = await createContext(req, res);
+    //  const users = await context.query.User.findMany();
+    //  res.json(users);
+    // });
+   },
+  },
+  files: {
+   upload: "local",
+   local: {
+    storagePath: filesStoragePath,
+    baseUrl: filesBaseUrl,
+   },
+  },
+  db: {
+   provider: "sqlite",
+   url: "file:../db/app.db",
+   useMigrations: true,
+  },
+  ui: {
+   isAccessAllowed: context => !!context.session?.data,
+  },
+  lists,
+  session,
+ })
 );
