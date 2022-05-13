c2933e9db0e6e6586e8c9965344edb4ae4961c72
Max Gaydenko <max@gaydenko.ru>
2022-05-13 13:38:22 +0300

    update video

--- front/src/components/App.tsx
+++ front/src/components/App.tsx
@@ -76,9 +76,9 @@ const App: React.FC<IProps> = (props: IProps) => {
  return (
   <div className={"App" + (homeMarker ? " App-home" : " App-page") + (menuShown ? " App-menu-shown" : "")}>
    <div className="layer-background-video">
-    <video id="video-home" poster={`/video/V0512ABG.jpg`} muted loop autoPlay playsInline>
-     <source type="video/mp4" src={`/video/V0512AOUT111.mp4`} />
-     <source type="video/webm" src={`/video/V0512AOUT111.webm`} />
+    <video id="video-home" poster={`/video/lf0513a.jpg`} muted loop autoPlay playsInline>
+     <source type="video/mp4" src={`/video/lf0513a.mp4`} />
+     <source type="video/webm" src={`/video/lf0513a.webm`} />
     </video>
    </div>
    <div className="layer-radial"></div>
