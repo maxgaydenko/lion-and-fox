576b1cfcf5e9bc8f24fd324bf86a0f745a3af68a
Max Gaydenko <max@gaydenko.ru>
2022-05-31 12:27:45 +0300

    Fix menu position

--- front/src/utils/struct.ts
+++ front/src/utils/struct.ts
@@ -15,7 +15,7 @@ export interface ISiteStructPage {
  readonly children: string[];
 }
 
-export type TSiteStructPagesMap = {[key: string]: ISiteStructPage}
+export type TSiteStructPagesMap = { [key: string]: ISiteStructPage };
 
 export interface ISiteStruct {
  readonly pages: TSiteStructPagesMap;
@@ -41,29 +41,27 @@ export interface IStructPageDataItem {
 // }
 
 export const combineSiteStruct = (dataItems: IStructPageDataItem[]): ISiteStruct => {
+ const menu: ISiteMenuItem[] = [];
  const _pageChildren: { [key: string]: string[] } = dataItems.reduce((p, c) => {
   p[c.url] = [];
   return p;
  }, {} as { [key: string]: string[] });
- dataItems
-  .forEach(c => {
-   if (c.parent !== null && _pageChildren[c.parent.url] !== undefined) _pageChildren[c.parent.url].push(c.url);
-  });
+ dataItems.sort(f => f.pos).forEach(c => {
+  if (c.showInMenu)
+   menu.push({ url: c.url, pos: c.pos < 1000 ? EMenuItemPosition.BeforePresentations : EMenuItemPosition.AfterPresentations });
+  if (c.parent !== null && _pageChildren[c.parent.url] !== undefined) _pageChildren[c.parent.url].push(c.url);
+ });
  const urls: string[] = [];
- const menu: ISiteMenuItem[] = [];
- const pages: TSiteStructPagesMap = dataItems
-  .reduce((p, c) => {
-   if (c.url[0] !== "@") urls.push(c.url);
-   if (c.showInMenu)
-    menu.push({ url: c.url, pos: c.pos < 1000 ? EMenuItemPosition.BeforePresentations : EMenuItemPosition.AfterPresentations });
-   p[c.url] = {
-    url: c.url,
-    title: c.menuName,
-    parent: c.parent !== null && _pageChildren[c.parent.url] !== undefined ? c.parent.url : null,
-    children: _pageChildren[c.url] !== undefined ? _pageChildren[c.url] : [],
-   };
-   return p;
-  }, {} as TSiteStructPagesMap);
+ const pages: TSiteStructPagesMap = dataItems.reduce((p, c) => {
+  if (c.url[0] !== "@") urls.push(c.url);
+  p[c.url] = {
+   url: c.url,
+   title: c.menuName,
+   parent: c.parent !== null && _pageChildren[c.parent.url] !== undefined ? c.parent.url : null,
+   children: _pageChildren[c.url] !== undefined ? _pageChildren[c.url] : [],
+  };
+  return p;
+ }, {} as TSiteStructPagesMap);
 
  return {
   menu,
